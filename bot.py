b00291c1b659e165099ea36312787f70e72da736
Zemka <51287499+Zemkaa@users.noreply.github.com>
2019-06-08 00:01:01 +0300

    Add files via upload
    New version of bot which adds buttons to the users with all directions of news we provide

--- bot.py
+++ bot.py
@@ -1,15 +1,27 @@
 import telebot
+from telebot import types
 bot = telebot.TeleBot('883226012:AAHkIxPBq2maVp9EQHLIpuDr8n60Pthbbq4')
 
 
-@bot.message_handler(content_types=['text'])
-def get_text_messages(message):
-    if message.text == "Hi":
+@bot.message(content_types=['text'])
+def direction_choice(message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(*[types.KeyboardButton(name) for name in ['Games', 'Business', 'Sport', 'Total news']])
+    msg = bot.send_message(message.chat.id, 'Choose the topic', reply_markup=keyboard)
+    bot.register_next_step_handler(msg, name)
+
+
+def name(message):
+    if message.text == 'Games':
         bot.send_message(message.from_user.id, "Hi, I am HottestNewsBot and now I'm in the development stage")
-    elif message.text == "/help":
-        bot.send_message(message.from_user.id, "Type Hi")
+    elif message.text == 'Business':
+        bot.send_message(message.from_user.id, "Hi, I am HottestNewsBot and now I'm in the development stage")
+    elif message.text == 'Sport':
+        bot.send_message(message.from_user.id, "Hi, I am HottestNewsBot and now I'm in the development stage")
+    elif message.text == 'Total news':
+        bot.send_message(message.from_user.id, "Hi, I am HottestNewsBot and now I'm in the development stage")
     else:
-        bot.send_message(message.from_user.id, "I can't understand you, type /help.")
+        bot.send_message(message.from_user.id, "I can't understand you, choose one of the topics.")
 
 
 bot.polling(none_stop=True, interval=0)
