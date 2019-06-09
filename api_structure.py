48da3395e23931af3ca51c5fcef0aff277149f1c
Zemka <51287499+Zemkaa@users.noreply.github.com>
2019-06-10 01:26:31 +0300

    Add files via upload

--- api_structure.py
+++ api_structure.py
@@ -1,11 +1,16 @@
 import requests
+import time
 
 
 def process_response(topic):
     token = '8b9f58ea8b9f58ea8b9f58eab98bf5a2d188b9f8b9f58ead767545b404b0c2293ca5e22'
     version = '5.95'
     domain_list = []
-    with open('file_domain.csv', 'r') as f:
+    current_time = time.time()
+    all_posts = []
+    most_liked1, most_liked2, most_liked3 = 0, 0, 0
+    most_liked1_index, most_liked2_index, most_liked3_index = 0, 0, 0
+    with open('file_domain.txt', 'r') as f:
         for row in f:
             parts = row.split(';')
             if parts[0] == topic:
@@ -20,5 +25,23 @@ def process_response(topic):
                                     'count': 100
                                 }
                                 )
-        data = response.json()['response']['item']
+        data = response.json()['response']['items']
+        all_posts.extend(data)
+        for i in range(100):
+            if all_posts[i]['likes']['count'] > most_liked1 and all_posts[i]['date'] >= current_time - 86400:
+                most_liked3 = most_liked2
+                most_liked3_index = most_liked2_index
+                most_liked2 = most_liked1
+                most_liked2_index = most_liked1_index
+                most_liked1 = all_posts
+                most_liked1_index = i
+            elif all_posts[i]['likes']['count'] > most_liked2 and all_posts[i]['date'] >= current_time - 86400:
+                most_liked3 = most_liked2
+                most_liked3_index = most_liked2_index
+                most_liked2 = most_liked1
+                most_liked2_index = i
+            elif all_posts[i]['likes']['count'] > most_liked3 and all_posts[i]['date'] >= current_time - 86400:
+                most_liked3 = all_posts[i]
+                most_liked3_index = i
+
 
